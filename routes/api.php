5643bb902a776084cabfb9ff843a0050354239f4
Khodor Hassoun <khodorhassoun@outlook.com>
2022-09-29 23:18:34 +0300

    Test if sort route is functional

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/sort/{string}', function($string){
+//     return 'Hello'.$string;
+// });
+Route::get('/sort/{string}', function($string){
+    return 'Hello '.$string;
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
